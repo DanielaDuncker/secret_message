7e0143c4f3f9513deb6304479ea5952f7bf86057
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-18 15:08:20 +0200

    i hate

--- secret_message.py
+++ secret_message.py
@@ -1,1 +1,14 @@
-print("hello")
+import os
+
+def rename_files():
+    file_list = os.listdir(r"/Users/Daniela/Downloads/prank/")
+    saved_path = os.getcwd()
+    print("Current Working Directory is "+saved_path)
+    os.chdir(r"/Users/Daniela/Downloads/prank/")
+
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "123456789"))
+    os.chdir(saved_path)
+
+rename_files()
+
