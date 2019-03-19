984397e634234bc078d1ff16e761a9e074c5efc9
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-19 12:21:42 +0200

    little progress

--- secret_message.py
+++ secret_message.py
@@ -1,14 +1,17 @@
 import os
 
+
 def rename_files():
-    file_list = os.listdir(r"/Users/Daniela/Downloads/prank/")
+    path = r"/Users/Daniela/Downloads/prank/"
+    file_list = os.listdir(path)
+    print(file_list)
     saved_path = os.getcwd()
-    print("Current Working Directory is "+saved_path)
+    print("Current Working Directory is " + saved_path)
     os.chdir(r"/Users/Daniela/Downloads/prank/")
 
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None, "123456789"))
+        os.rename(file_name, file_name.translate(str.maketrans("0123456789", ",,,,,,,,,,")))
     os.chdir(saved_path)
 
-rename_files()
 
+rename_files()
