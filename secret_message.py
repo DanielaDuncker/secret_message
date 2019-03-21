4a46af09cac22069518ff95337d37f0105e567af
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-21 09:44:31 +0200

    kaikki muuten toimii paitsi et ne ei mee aakkosjärjestykseen

--- secret_message.py
+++ secret_message.py
@@ -2,16 +2,17 @@ import os
 
 
 def rename_files():
-    path = r"/Users/Daniela/Downloads/prank/"
+    path = r"/Users/Daniela/Downloads/prank 2"
     file_list = os.listdir(path)
     print(file_list)
     saved_path = os.getcwd()
-    print("Current Working Directory is " + saved_path)
-    os.chdir(r"/Users/Daniela/Downloads/prank/")
+    print("Current Wrorking Directory is " + saved_path)
+    os.chdir(r"/Users/Daniela/Downloads/prank 2")
 
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(str.maketrans("0123456789", ",,,,,,,,,,")))
+        print("Old Name - " + file_name)
+        print("New Name - " + file_name.translate(str.maketrans("", "", "0123456789")))
+        os.renames(file_name, file_name.translate(str.maketrans("", "", "0123456789")))
     os.chdir(saved_path)
-
 
 rename_files()
