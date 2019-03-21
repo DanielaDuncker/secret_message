0d7ea22b44ba1097713eab5f0b147055201a0302
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-21 09:47:57 +0200

    meh

--- secret_message.py
+++ secret_message.py
@@ -6,7 +6,7 @@ def rename_files():
     file_list = os.listdir(path)
     print(file_list)
     saved_path = os.getcwd()
-    print("Current Wrorking Directory is " + saved_path)
+    print("Current Working Directory is " + saved_path)
     os.chdir(r"/Users/Daniela/Downloads/prank 2")
 
     for file_name in file_list:
@@ -14,5 +14,6 @@ def rename_files():
         print("New Name - " + file_name.translate(str.maketrans("", "", "0123456789")))
         os.renames(file_name, file_name.translate(str.maketrans("", "", "0123456789")))
     os.chdir(saved_path)
+
 
 rename_files()
